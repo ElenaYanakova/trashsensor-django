68000b765e402ee5489f6f944518d46817eb1191
repitch <ilyapya2@gmail.com>
2017-01-19 00:31:30 +0300

    Settings and required apps

--- trashsensor/settings.py
+++ trashsensor/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'trashsensor.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'tunemate_trash10',  # Or path to database file if using sqlite3.
+        'USER': 'tunemate_trash10',  # Not used with sqlite3.
+        'PASSWORD': 'WEmZcf9Rgw',  # Not used with sqlite3.
+        'HOST': 'mmysql7.locum.ru',  # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',  # Set to empty string for default. Not used with sqlite3.
     }
 }
 
